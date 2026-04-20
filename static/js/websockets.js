ab9cb6d3f9412e6951ac79311c272c6be0f4e499
rachel-mp4 <rachel@moth11.net>
2026-04-20 16:50:13 -0400

    lazy fix to race

--- static/js/websockets.js
+++ static/js/websockets.js
@@ -1,29 +1,31 @@
-const wantsNewThreads = localStorage.getItem("new-threads")
-const baseurl = window.location.hostname
-const port = window.location.port
-const proto = window.location.protocol
-const withproto = `ws${proto.slice(4)}${baseurl}${(port !== "") ? ":" + port : ""}/ws?watcher=${wantsNewThreads === "" ? "yes" : "and-new-threads"}`
-const path = window.location.pathname
-const match = path.match(/^\/t\/([0-9a-z]+)/)
-if (match !== null) {
-  const ntid = match[1]
-  wsurl = `${withproto}&wormwatch=${ntid}&thread=${ntid}`
-} else {
-  wsurl = withproto
-}
-const ws = new WebSocket(wsurl)
-ws.onopen = () => { console.log("hello ws") }
-ws.onmessage = (e) => {
-  const jed = JSON.parse(e.data)
-  const ev = new CustomEvent(`cbv:${jed.type}`, { detail: jed.data })
-  document.dispatchEvent(ev)
-}
-ws.onerror = (e) => {
-  console.log("wserror,", e)
-}
-ws.onclose = (e) => {
-  console.log("wsclose,", e)
-}
-if (wantsNewThreads === null) {
-  localStorage.setItem("new-threads", "yes")
-}
+setTimeout(() => {
+  const wantsNewThreads = localStorage.getItem("new-threads")
+  const baseurl = window.location.hostname
+  const port = window.location.port
+  const proto = window.location.protocol
+  const withproto = `ws${proto.slice(4)}${baseurl}${(port !== "") ? ":" + port : ""}/ws?watcher=${wantsNewThreads === "" ? "yes" : "and-new-threads"}`
+  const path = window.location.pathname
+  const match = path.match(/^\/t\/([0-9a-z]+)/)
+  if (match !== null) {
+    const ntid = match[1]
+    wsurl = `${withproto}&wormwatch=${ntid}&thread=${ntid}`
+  } else {
+    wsurl = withproto
+  }
+  const ws = new WebSocket(wsurl)
+  ws.onopen = () => { console.log("hello ws") }
+  ws.onmessage = (e) => {
+    const jed = JSON.parse(e.data)
+    const ev = new CustomEvent(`cbv:${jed.type}`, { detail: jed.data })
+    document.dispatchEvent(ev)
+  }
+  ws.onerror = (e) => {
+    console.log("wserror,", e)
+  }
+  ws.onclose = (e) => {
+    console.log("wsclose,", e)
+  }
+  if (wantsNewThreads === null) {
+    localStorage.setItem("new-threads", "yes")
+  }
+}, 1500)
